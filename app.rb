4c16c1ab3cbd1dbf2e3291d85a631af9aef1221a
Stacy1999 <anastasiapolinesi@gmail.com>
2021-02-16 11:45:18 +0000

    Refactored using MVC pattern

--- app.rb
+++ app.rb
@@ -1,10 +1,12 @@
 require 'sinatra/base'
+require './lib/bookmark'
 
 class BookmarkManager < Sinatra::Base
-  get '/' do
-    'Bookmark Manager'
-  end
 
+  get '/bookmarks' do
+    @bookmarks = Bookmark.all
+   erb :'bookmarks/index'
+  end
 
 
 
